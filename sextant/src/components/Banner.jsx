0c71e8e0114258a82dafb7fbf8ae02c8878bdef1
Krushna Thakare <krushnathakare@Krushnas-MacBook-Air.local>
2024-12-31 19:37:52 +0530

    Banner component

---
+++ sextant/src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import './Banner.css'
+const Banner = () => {
+   
+    return (
+        <>
+        <div id='banner'>
+            <h1>Sextant Dashboard</h1>
+        </div>
+            
+    </>
+    )
+}
+
+export default Banner;
